65614a714bb28a94e1827465859512e95d09513c
Samuel Kyletoft <skyletoft@gmail.com>
2020-04-04 23:47:47 +0200

    Updated to work with GNOME 3.36

--- extension.js
+++ extension.js
@@ -4,18 +4,19 @@ let SavedstartSearch;
 let SavedonTextChanged;
 
 function init() {
-	this.SavedstartSearch = Main.overview._controls.viewSelector.startSearch;
-	this.SavedonTextChanged = Main.overview._controls.viewSelector._onTextChanged;
+	this.SavedstartSearch = Main.overview.viewSelector.startSearch;
+	this.SavedonTextChanged = Main.overview.viewSelector._onTextChanged;
 }
 
 function enable() {
-	Main.overview._searchEntryBin.hide();
-	Main.overview._controls.viewSelector.startSearch = function(event) {};
-	Main.overview._controls.viewSelector._onTextChanged = function(se, prop) {};
+	Main.overview.searchEntry.hide();
+	Main.overview.viewSelector.startSearch = function(event) {};
+	Main.overview.viewSelector._onTextChanged = function(se, prop) {};
 }
 
 function disable() {
-	Main.overview._searchEntryBin.show();
-	Main.overview._controls.viewSelector.startSearch = this.SavedstartSearch;
-	Main.overview._controls.viewSelector._onTextChanged = this.SavedonTextChanged;
+	Main.overview.searchEntry.show();
+	Main.overview.viewSelector.startSearch = this.SavedstartSearch;
+	Main.overview.viewSelector._onTextChanged = this.SavedonTextChanged;
 }
+
